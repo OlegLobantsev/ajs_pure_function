0d818b30f557764479e06a821bbc24d46505a468
Oleg Lobantsev <j.legis@gmail.com>
2023-02-28 15:56:20 +0300

    some fix

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -2,8 +2,8 @@ import playerHealth from '../app';
 
 test.each([
   [{ name: 'Маг', health: 51 }, 'healthy'],
-  [{ name: 'Боец', health: 16 }, 'wounded'],
-  [{ name: 'Черт', health: 1 }, 'critical'],
+  [{ name: 'Мечник', health: 16 }, 'wounded'],
+  [{ name: 'Лучник', health: 1 }, 'critical'],
 ])('return string', (obj, expected) => {
   const result = playerHealth(obj);
   expected(result).toBe(expected);
